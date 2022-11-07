8e14500cfe55a01fec12223f58cbc7669d7a4005
Gavin <gavinporlich@gmail.com>
2022-11-07 01:38:21 -0600

    started navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
         </a>
-      </header>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+<button type="button" className="nav-item btn" onClick={() => setHomeDisplay("about")}>About Me</button>
+            </li>
+            <li className="nav-item">
+<button type="button" className="nav-item btn" onClick={() => setHomeDisplay("proj")}>My Work</button>
+            </li>
+            <li className="nav-item">
+<button type="button" className="nav-item btn" onClick={() => setHomeDisplay("contact")}>Contact Info</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ src/components/navContainer.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+
+export default function navContainer() {
+    const [selectedNav, setSelectedNav] = useState('home');
+
+};
